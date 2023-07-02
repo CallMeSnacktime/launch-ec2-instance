7ce405c8e1b828faaee541907e9df4d95aa9b60e
CallMeSnacktime <teebz25@gmail.com>
2023-07-02 14:54:13 -0400

    Update

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,15 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform-user"
-}
-
-
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
-    key     = "build/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "terraform-user"
-  }
+  region    = "us-east-1"
+  profile   = "terraform-user"
 }
 
 
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
 
-  tags = {
-    Name = "default vpc"
+  tags    = {
+    Name  = "default vpc"
   }
 }
 
@@ -28,12 +17,12 @@ resource "aws_default_vpc" "default_vpc" {
 # use data source to get all avalablility zones in region
 data "aws_availability_zones" "available_zones" {}
 
-
+ 
 # create default subnet if one does not exit
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.available_zones.names[0]
 
-  tags = {
+  tags   = {
     Name = "default subnet"
   }
 }
@@ -46,29 +35,29 @@ resource "aws_security_group" "ec2_security_group" {
   vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
-    description = "http access"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "http access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "ssh access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["152.208.12.118/32"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags   = {
     Name = "ec2 security group"
   }
 }
@@ -78,7 +67,7 @@ resource "aws_security_group" "ec2_security_group" {
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
-
+  
   filter {
     name   = "owner-alias"
     values = ["amazon"]
@@ -97,16 +86,16 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "LabWork"
   user_data              = file("install_techmax.sh")
 
   tags = {
-    Name = "techmax server"
+    Name = "techmax sever"
   }
 }
 
 
-# print the url of the server
-output "ec2_public_ipv4_url" {
-  value = join("", ["http://", aws_instance.ec2_instance.public_ip])
+# print the ec2's public ipv4 address
+output "public_ipv4_address" {
+  value = join("",["http://",aws_instance.ec2_instance.public_ip])
 }
